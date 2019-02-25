3f3fecf36efa4c61a6cfe4045bd8097e3d067d75
NET\flobo <fluvio.lobo@gmail.com>
2019-02-25 12:22:31 -0500

    Began working on an Arduino base program for this and future projects involving MQtt

---
+++ Software/Arduino/com/com.ino
@@ -0,0 +1,36 @@
+/*
+ * test
+ */
+
+// Useful defines.
+#define BAUDRATE              115200                          // Serial communication baudrate
+
+// Call auxiliary functions library
+#include "MqttFunctions.h"
+
+void setup() {
+
+  Serial.begin( BAUDRATE );                                   // Start serial monitor
+
+  /*  Connect to Wi-Fi and setup MQTT communications */
+  setup_WiFi();                                               // Connect to WiFi
+  MQTT_connect( 250 );                                        // Connecto to MQTT broker
+
+} // end of setup loop...
+
+void loop() {
+
+  // connection stuff...
+  if ( !mqtt.connected() )                                    // Ensure we are connected
+    MQTT_connect( 125 );                                      // to MQTT server
+
+  // do stuff with sensors here...
+  char    buff[156] = {'\0'};                                 // String buffer
+  // connection stuff...
+  if ( devOutput.publish( buff ) )                              // Make sure it publishes
+    Serial.println( buff );                                     // ...
+  else                                                          // In case it fails, disconnect from MQTT Server
+    mqtt.disconnect();                                          // ...
+
+  delay( 10 );
+}
