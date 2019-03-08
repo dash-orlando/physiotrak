69d83e9b9a54ae31a90ccafd1599e380ea17b625
madelenehabib <madelenehabib@knights.ucf.edu>
2019-03-08 14:26:41 -0500

    uploading my version of MQTT and heart rate monitor code

---
+++ Software/Arduino/MQTT MH/mqtt_MHF/mqtt_MHF.ino
@@ -0,0 +1,124 @@
+ /*
+ * test
+ */
+
+ /*Madelene */
+
+// Heart Rate Call auxiliary functions library
+#include "MQTT_Functions.h"
+
+//Heart Rate Call i2c library
+#include <Wire.h>
+
+//Heart Rate Call sparkfun Heartrate sensor library?
+#include "MAX30105.h"
+#include "heartRate.h"
+
+// Useful defines.
+#define BAUDRATE              115200                          // Serial communication baudrate
+
+//HeartRate
+MAX30105 particleSensor;
+
+// Variables
+//int buff; //Fluvio's buff
+
+int irValue;
+
+//HeartRate Variables 
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good. 
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occured 
+float beatsPerMinute;
+int beatAvg;
+
+// String def.
+String data_out_one = "IR=";
+String data_out_two = "irValue";
+//heart_rate_data[2] = ",BPM=";
+//heart_rate_data[3] = "beatsPerMinute";
+//heart_rate_data[4] = ", Avg BPM=";
+//heart_rate_data[5] = "beatAvg";
+
+
+void setup() 
+{
+
+  Serial.begin( BAUDRATE );                                   // Start serial monitor
+
+  /*  Connect to Wi-Fi and setup MQTT communications */
+  setup_WiFi();                                               // Connect to WiFi
+  MQTT_connect( 250 );                                        // Connecto to MQTT broker
+
+//Heart Rate void setup
+Serial.println("Initializing..."); 
+//Initialize sensor
+if (!particleSensor.begin(Wire, I2C_SPEED_FAST))//Use default I2C port, 400Hz speed
+{
+  Serial.println("MAX30105 was not found. Please check wiring/power. ");
+  while (1);
+}
+Serial.println("Place your index finger on the sensor with steady pressure.");
+
+particleSensor.setup(); //Configure sensor with default settings
+particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running 
+particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED 
+
+
+} 
+// end of setup loop...
+
+
+
+void loop() {
+
+  // connection stuff...
+  if ( !mqtt.connected() )                                    // Ensure we are connected
+    MQTT_connect( 125 );                                      // to MQTT server
+
+  // do stuff with sensors here...
+  irValue = particleSensor.getIR();
+
+  int irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+
+    Serial.println(data_out_one + data_out_two);
+
+    if (irValue < 50000)
+        Serial.print(" No finger?");
+    
+      Serial.println();
+    }
+
+  
+  // connection stuff...
+  if ( devOutput.publish( irValue ) ) {                              // Make sure it publishes
+    Serial.println( irValue );
+  } else {                                                          // In case it fails, disconnect from MQTT Server
+    mqtt.disconnect();                                          // ...
+  }
+
+  delay( 10 );
+}
+
+  
